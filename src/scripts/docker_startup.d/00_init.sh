bc1ea1b53086b32c9edea520e2716126b31c50db
Mathias <email@example.com>
2024-02-15 09:34:49 +0100

    save

---
+++ src/scripts/docker_startup.d/00_init.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+SCRIPT_NAME_W_FULL_PATH=$(readlink -f $0);
+echo "run script => $SCRIPT_NAME_W_FULL_PATH";
---
+++ src/scripts/docker_startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set +x
+SCRIPT_FOLDER="/docker_startup.d"
+for SCRIPT in $(ls "$SCRIPT_FOLDER" | sort -g)
+do
+out=$($SCRIPT_FOLDER/$SCRIPT 2>$? >>/tmp/output.txt); rc=$?;
+echo "RUN SCRIPT $SCRIPT EXIT-CODE => $rc"
+if  [ "$rc" -ne "0" ]; then
+    echo "error $rc in script $SCRIPT: $out" >&2
+    exit $rc
+fi
+done
+wait
