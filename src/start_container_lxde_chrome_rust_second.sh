9ccbba4166aa8ba3a187f9cb27af3bd5fc7c2515
Mathias <email@example.com>
2024-03-25 11:59:50 +0100

    save /w hint for dbus

--- src/start_container_lxde_chrome_rust_second.sh
+++ src/start_container_lxde_chrome_rust_second.sh
@@ -17,10 +17,32 @@
 # debian_lxde_chrome bash
 
 # /w sound
+# docker run -it \
+# --security-opt apparmor=unconfined \
+# --cap-add SYS_ADMIN \
+# --cap-add CAP_SYSLOG \
+# --tmpfs /run \
+# --volume /sys/fs/cgroup:/sys/fs/cgroup \
+# --volume /dev/shm:/dev/shm \
+# --device /dev/snd \
+# --device /dev/dri \
+# --volume /sys/fs/cgroup:/sys/fs/cgroup \
+# --volume /dev/shm:/dev/shm \
+# --volume ./workspace:/home/user/workspace:rw \
+# --env PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+# --volume ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+# --volume ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+# --group-add $(getent group audio | cut -d: -f3) \
+# debian_lxde_chrome_rust_second bash
+
+
 docker run -it \
 --security-opt apparmor=unconfined \
 --cap-add SYS_ADMIN \
 --cap-add CAP_SYSLOG \
+--cap-add CAP_PERFMON \
+--cap-add CAP_BPF \
+--cap-add CAP_CHECKPOINT_RESTORE \
 --tmpfs /run \
 --volume /sys/fs/cgroup:/sys/fs/cgroup \
 --volume /dev/shm:/dev/shm \
@@ -35,7 +57,6 @@ docker run -it \
 --group-add $(getent group audio | cut -d: -f3) \
 debian_lxde_chrome_rust_second bash
 
-
 # start script
 # sudo sh +x ./start_container_lxde_chrome.sh
 
@@ -43,3 +64,5 @@ debian_lxde_chrome_rust_second bash
 # ps axww
 # ps aaxwwue
 # ps -efw
+
+# CAP_PERFMON, CAP_BPF, and CAP_CHECKPOINT_RESTORE
