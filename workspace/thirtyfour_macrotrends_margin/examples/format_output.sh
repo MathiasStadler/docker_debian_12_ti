8a8c6ccc013dfa214b4b5e891f5f1b838b9d7aa4
Mathias <email@example.com>
2024-06-24 13:08:29 +0200

    execute script funzt

---
+++ workspace/thirtyfour_macrotrends_margin/examples/format_output.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# copy first output file to test.txt
+sed -i 's/\\x22/"/g' test.txt
+sed -i 's/\\x27/0x/g' test.txt
+sed -i 's/\\x3e/</g' test.txt
+sed -i 's/\\x3c/>/g' test.txt
+sed -i 's/\\x3d/=/g' test.txt
+sed -i 's/\\x3db/*/g' test.txt
+sed -i 's/\\x3db/*/g' test.txt
+sed -i  's/</\r\n\n</g' test.txt
+sed -i 's/</\r\n</g' test.txt
+sed -i 's/<div/\r\n<div/g'  test.txt
