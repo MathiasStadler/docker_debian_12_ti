ab08a28ec494a6c5f7d2931d40c48d0d1271695e
Mathias Stadler <stadler-mathias@web.de>
2024-07-02 15:50:06 +0200

    finished for today

--- src/start_container_lxde_chromium_rust_raspi.sh
+++ src/start_container_lxde_chromium_rust_raspi.sh
@@ -58,7 +58,7 @@ docker run -it \
 debian_lxde_chromium_rust_raspi bash
 
 // raspi host
-docker run -it \
+sudo docker run -it \
 --security-opt apparmor=unconfined \
 --cap-add SYS_ADMIN \
 --cap-add CAP_SYSLOG \
@@ -78,6 +78,31 @@ docker run -it \
 --group-add "$(getent group audio | cut -d: -f3)" \
 debian_lxde_chromium_rust_raspi bash
 
+
+# --privileged 
+
+// raspi host
+sudo docker run -it \
+--security-opt apparmor=unconfined \
+--cap-add SYS_ADMIN \
+--cap-add CAP_SYSLOG \
+--cap-add CAP_PERFMON \
+--cap-add CAP_BPF \
+--cap-add CAP_CHECKPOINT_RESTORE \
+--tmpfs /run \
+--volume /dev/shm:/dev/shm \
+--device /dev/snd \
+--device /dev/dri \
+--volume /sys/fs/cgroup:/sys/fs/cgroup:rw \
+--volume /dev/shm:/dev/shm \
+--volume /home/trapapa/docker_debian_12_ti/workspace:/home/user/workspace:rw \
+--env PULSE_SERVER=unix:"${XDG_RUNTIME_DIR}"/pulse/native \
+--volume "${XDG_RUNTIME_DIR}"/pulse/native:"${XDG_RUNTIME_DIR}"/pulse/native \
+--volume ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+--group-add "$(getent group audio | cut -d: -f3)" \
+--privileged  \
+--userns host \
+debian_lxde_chromium_rust_raspi bash
 
 # start script
 # sudo sh +x ./start_container_lxde_chrome.sh
